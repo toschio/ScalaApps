c6112475d83bd074b6dd237c7f19d953fbc60a74
Aj <ajaykuma24@gmail.com>
2020-01-21 20:24:19 +0100

    updated

--- Spark_Scala/SparkApplicationsSetup/ScalaCodesExamples/Code13.scala
+++ Spark_Scala/SparkApplicationsSetup/ScalaCodesExamples/Code13.scala
@@ -3,8 +3,13 @@ object Code13 {
  def main(args: Array[String]){
   var randSent = "hi how are you"
   val randArray = randSent.toArray
+  val randArray1 = randSent.split(" ")
+  print("first array\n")
   for (v <- randArray)
-  print(v)
+  println(v)
+  print("second array\n")
+  for (v <- randArray1)
+  println(v)
 
 }
 }
